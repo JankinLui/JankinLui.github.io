c021c5ef8dda9fc8edc46e0e4d8f2e38b4c7ae72
JankinLui <13164646190@163.com>
2021-12-19 23:27:23 +0800

    Site updated: 2021-12-19 23:27:23

--- dist/music.js
+++ dist/music.js
@@ -4,21 +4,21 @@ const ap = new APlayer({
     autoplay: false,
     audio: [
       {
-        name: "PDD洪荒之力",
-        artist: '徐梦圆',
-        url: 'http://up.mcyt.net/?down/39868.mp3',
+        name: '星晴',
+        artist: '周杰伦',
+        url: 'https://link.jscdn.cn/sharepoint/aHR0cHM6Ly8xZHJpdi1teS5zaGFyZXBvaW50LmNvbS86dTovZy9wZXJzb25hbC9zdG9yXzFkcml2X29ubWljcm9zb2Z0X2NvbS9FYTVoYjFhOTNrRkJzLUJZSFRNV05fWUI5eTI5NnprOEhPNzVpazkxM0dtSkln.mp3',
         cover: 'http://oeff2vktt.bkt.clouddn.com/image/84.jpg',
       },
       {
-        name: '9420',
-        artist: '麦小兜',
-        url: 'http://up.mcyt.net/?down/45967.mp3',
+        name: '所念皆星河',
+        artist: '纯音乐',
+        url: 'https://link.jscdn.cn/sharepoint/aHR0cHM6Ly8xZHJpdi1teS5zaGFyZXBvaW50LmNvbS86dTovZy9wZXJzb25hbC9zdG9yXzFkcml2X29ubWljcm9zb2Z0X2NvbS9FVktPSDRiQXp6QkRtVG0yRlZXUF9UMEJOMUpMdFgtd01iNUhwTWMydFVLcDVB.mp3',
         cover: 'http://oeff2vktt.bkt.clouddn.com/image/8.jpg',
       },
       {
-        name: '风筝误',
-        artist: '刘珂矣',
-        url: 'http://up.mcyt.net/?down/46644.mp3',
+        name: '打上花火',
+        artist: 'Cover DAOKO _ 米津玄師',
+        url: 'https://link.jscdn.cn/sharepoint/aHR0cHM6Ly8xZHJpdi1teS5zaGFyZXBvaW50LmNvbS86dTovZy9wZXJzb25hbC9zdG9yXzFkcml2X29ubWljcm9zb2Z0X2NvbS9FV3dIRWNuSXoyaEdzbVZxM2Zfb2EyNEItNlhJelRCMm90VWhqZzZWLWx4Yk9B.mp3',
         cover: 'http://oeff2vktt.bkt.clouddn.com/image/96.jpg',
       }
     ]
