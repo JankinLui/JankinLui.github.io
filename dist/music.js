4b0ea4f1bbf08d9ff9decb26b1d460a332a43a06
JankinLui <13164646190@163.com>
2021-12-19 23:36:05 +0800

    Site updated: 2021-12-19 23:36:05

--- dist/music.js
+++ dist/music.js
@@ -7,19 +7,19 @@ const ap = new APlayer({
         name: '星晴',
         artist: '周杰伦',
         url: 'https://link.jscdn.cn/sharepoint/aHR0cHM6Ly8xZHJpdi1teS5zaGFyZXBvaW50LmNvbS86dTovZy9wZXJzb25hbC9zdG9yXzFkcml2X29ubWljcm9zb2Z0X2NvbS9FYTVoYjFhOTNrRkJzLUJZSFRNV05fWUI5eTI5NnprOEhPNzVpazkxM0dtSkln.mp3',
-        cover: 'http://oeff2vktt.bkt.clouddn.com/image/84.jpg',
+        cover: '',
       },
       {
         name: '所念皆星河',
         artist: '纯音乐',
         url: 'https://link.jscdn.cn/sharepoint/aHR0cHM6Ly8xZHJpdi1teS5zaGFyZXBvaW50LmNvbS86dTovZy9wZXJzb25hbC9zdG9yXzFkcml2X29ubWljcm9zb2Z0X2NvbS9FVktPSDRiQXp6QkRtVG0yRlZXUF9UMEJOMUpMdFgtd01iNUhwTWMydFVLcDVB.mp3',
-        cover: 'http://oeff2vktt.bkt.clouddn.com/image/8.jpg',
+        cover: '',
       },
       {
         name: '打上花火',
         artist: 'Cover DAOKO _ 米津玄師',
         url: 'https://link.jscdn.cn/sharepoint/aHR0cHM6Ly8xZHJpdi1teS5zaGFyZXBvaW50LmNvbS86dTovZy9wZXJzb25hbC9zdG9yXzFkcml2X29ubWljcm9zb2Z0X2NvbS9FV3dIRWNuSXoyaEdzbVZxM2Zfb2EyNEItNlhJelRCMm90VWhqZzZWLWx4Yk9B.mp3',
-        cover: 'http://oeff2vktt.bkt.clouddn.com/image/96.jpg',
+        cover: '',
       }
     ]
 });
