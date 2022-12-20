7023f9ba456f414e58fdeb5344099f8216cf8abe
JankinLui <13164646190@163.com>
2022-12-20 13:29:38 +0800

    Site updated: 2022-12-20 13:29:37

--- js/welcome.js
+++ js/welcome.js
@@ -1,6 +1,6 @@
 //首次访问弹窗
 if (localStorage.getItem("popWelcomeWindow") != "0") {
-    if(document.referrer==undefined||document.referrer.indexOf("yisous.xyz")!=-1||document.referrer.indexOf("ariasaka.top")!=-1){ //改成自己域名，注意是referrer!!! qwq
+    if(document.referrer==undefined||document.referrer.indexOf("blog.cndis.cn")!=-1||document.referrer.indexOf("blog.cndis.cn")!=-1){ //改成自己域名，注意是referrer!!! qwq
         Snackbar.show({
             pos: "top-right",
             showAction: false,
